a7844e6879e4b903b3ddb3860ffdc459b2f89e20
chrisdancee <chris_j_dance@hotmail.com>
2016-03-10 11:44:00 +0000

    Update my-bets-collection.js

--- js/collections/my-bets-collection.js
+++ js/collections/my-bets-collection.js
@@ -6,6 +6,6 @@ var BedeCasino = BedeCasino || {};
 // The url would be used on a POST, as the https://bedefetechtest.herokuapp.com/v1/ wasn't working I set up a local server for testing
 BedeCasino.MyBetsCollection = Backbone.Collection.extend({
   model: BedeCasino.MyBetModel,
-  //url: 'https://bedefetechtest.herokuapp.com/v1/'
-  url: 'http://localhost:3000/bets'
+  url: 'https://bedefetechtest.herokuapp.com/v1/'
+  //url: 'http://localhost:3000/bets'
 });
